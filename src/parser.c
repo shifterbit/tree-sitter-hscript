eb003f9e4a4dfe5280f964cf9afbb9949c78a569
shifterbit <48891590+shifterbit@users.noreply.github.com>
2026-09-22 12:39:53 -0400

    fix windows arm publishing

--- src/parser.c
+++ src/parser.c
@@ -28589,7 +28589,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_hscript(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 0,
-      .patch_version = 2,
+      .patch_version = 3,
     },
   };
   return &language;
